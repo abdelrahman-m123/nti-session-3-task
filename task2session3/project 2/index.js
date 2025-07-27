538f1115c68990f5e20226bba30047c0e69e17b2
Abdelrahman Mostafa Emad <123787144+abdelrahman-m123@users.noreply.github.com>
2025-07-27 12:44:02 +0300

    Update index.js

--- task2session3/project 2/index.js
+++ task2session3/project 2/index.js
@@ -4,12 +4,12 @@ const listCart = require('./modules/listCart');
 const calculateTotal = require('./modules/calculateTotal');
 
 async function main() {
-  // Example usage
-  await addToCart(1, 2); // Add product with ID 1, quantity 2
-  await addToCart(3, 1); // Add product with ID 3, quantity 1
+  
+  await addToCart(1, 2);
+  await addToCart(3, 1); 
   await listCart();
   await calculateTotal();
-  await removeFromCart(1, 1); // Remove 1 quantity of product with ID 1
+  await removeFromCart(1, 1); 
   await listCart();
   await calculateTotal();
 }
